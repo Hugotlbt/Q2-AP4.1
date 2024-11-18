75decb4ddc7d08d0744976d26641041b15c681de
Hugotlbt <hugo.tlbt@gmail.com>
2024-11-18 15:18:41 +0100

    u

--- Controleur/Controleur_Gerer_monCompte.php
+++ Controleur/Controleur_Gerer_monCompte.php
@@ -8,13 +8,14 @@ use App\Vue\Vue_Menu_Administration;
 use App\Vue\Vue_Structure_BasDePage;
 use App\Vue\Vue_Structure_Entete;
 use App\Vue\Vue_Utilisateur_Changement_MDP;
+require_once __DIR__.'/../src/Fonctions/fonctions.php';
 
 
 switch ($action) {
     case "changerMDP":
         //Il a cliqué sur changer Mot de passe. Cas pas fini
         $Vue->setEntete(new Vue_Structure_Entete());
-        $Vue->setMenu(new Vue_Menu_Administration());
+        $Vue->setMenu(new Vue_Menu_Administration($_SESSION["typeConnexionBack"]));
         $Vue->addToCorps(new Vue_Utilisateur_Changement_MDP("", "Gerer_monCompte"));
         break;
     case "submitModifMDP":
@@ -22,22 +23,28 @@ switch ($action) {
         $utilisateur = Modele_Utilisateur::Utilisateur_Select_ParId($_SESSION["idUtilisateur"]);
         if ($_REQUEST["AncienPassword"] == $utilisateur["motDePasse"])
         {
-            //on vérifie si le mot de passe de la BDD est le même que celui rentré
-            if ($_REQUEST["NouveauPassword"] == $_REQUEST["ConfirmPassword"]) {
-                $Vue->setEntete(new Vue_Structure_Entete());
-                $Vue->setMenu(new Vue_Menu_Administration());
-                Modele_Utilisateur::Utilisateur_Modifier_motDePasse($_SESSION["idUtilisateur"], $_REQUEST["NouveauPassword"]);
-                $Vue->addToCorps(new Vue_Compte_Administration_Gerer("<label><b>Votre mot de passe a bien été modifié</b></label>"));
-                // Dans ce cas les mots de passe sont bons, il est donc modifier
+            if (\App\Fonctions\CalculComplexiteMdp($_REQUEST["NouveauPassword"]) >= 90) {
+                //on vérifie si le mot de passe de la BDD est le même que celui rentré
+                if ($_REQUEST["NouveauPassword"] == $_REQUEST["ConfirmPassword"]) {
+                    $Vue->setEntete(new Vue_Structure_Entete());
+                    $Vue->setMenu(new Vue_Menu_Administration($_SESSION["typeConnexionBack"]));
+                    Modele_Utilisateur::Utilisateur_Modifier_motDePasse($_SESSION["idUtilisateur"], $_REQUEST["NouveauPassword"]);
+                    $Vue->addToCorps(new Vue_Compte_Administration_Gerer("<label><b>Votre mot de passe a bien été modifié</b></label>"));
+                    // Dans ce cas les mots de passe sont bons, il est donc modifier
 
-            } else {
+                } else {
+                    $Vue->setEntete(new Vue_Structure_Entete());
+                    $Vue->setMenu(new Vue_Menu_Administration($_SESSION["typeConnexionBack"]));
+                    $Vue->addToCorps(new Vue_Utilisateur_Changement_MDP("<label><b>Les nouveaux mots de passe ne sont pas identiques</b></label>", "Gerer_monCompte"));
+                }
+            }else{
                 $Vue->setEntete(new Vue_Structure_Entete());
-                $Vue->setMenu(new Vue_Menu_Administration());
-                $Vue->addToCorps(new Vue_Utilisateur_Changement_MDP("<label><b>Les nouveaux mots de passe ne sont pas identiques</b></label>", "Gerer_monCompte"));
+                $Vue->setMenu(new Vue_Menu_Administration($_SESSION["typeConnexionBack"]));
+                $Vue->addToCorps(new Vue_Utilisateur_Changement_MDP("<label><b>Le mot de passe saisi n'est pas assez sécurisé</b></label>", "Gerer_monCompte"));
             }
         } else {
             $Vue->setEntete(new Vue_Structure_Entete());
-            $Vue->setMenu(new Vue_Menu_Administration());
+            $Vue->setMenu(new Vue_Menu_Administration($_SESSION["typeConnexionBack"]));
             $Vue->addToCorps(new Vue_Utilisateur_Changement_MDP("<label><b>Vous n'avez pas saisi le bon mot de passe</b></label>", "Gerer_monCompte"));
         }
         break;
@@ -51,7 +58,7 @@ switch ($action) {
     default:
         //Cas par défaut: affichage du menu des actions.
         $Vue->setEntete(new Vue_Structure_Entete());
-        $Vue->setMenu(new Vue_Menu_Administration());
+        $Vue->setMenu(new Vue_Menu_Administration($_SESSION["typeConnexionBack"]));
         $Vue->addToCorps(new Vue_Compte_Administration_Gerer());
         break;
 }
